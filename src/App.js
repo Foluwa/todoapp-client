b4384eb6d087ddee67388d46948e810494d8a731
MORONFOLUWA <moronfoluwaakintola@gmail.com>
2021-09-26 02:21:57 +0100

    Implemeted Auth, Add and Delete Todo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,100 @@
+import React, { useEffect, useState } from 'react';
+import "antd/dist/antd.css";
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import SignIn from './pages/Authentication/SignIn';
+import SignUp from './pages/Authentication/SignUp';
+import Dashboard from './pages/Dashboard/Dashboard';
 
-const App =() => {
+import "react-toastify/dist/ReactToastify.css";
+import { toast } from "react-toastify";
+
+toast.configure();
+
+const App = () => {
+  const checkAuthenticated = async () => {
+    try {
+
+      console.log('localStorage.token ', localStorage.token)
+      const res = await fetch("http://localhost:3001/me", {
+        method: "GET",
+        headers: { Authorization: `Bearer ${localStorage.token}` }
+      });
+
+      const parseRes = await res.json();
+      console.log('parseRes RESPOSNE ', parseRes)
+      if (parseRes.email === true) { console.log('USER FOUND') } else { console.log('USER NOT FOUND') }
+
+      //parseRes === true ? setIsAuthenticated(true) : setIsAuthenticated(false);
+      parseRes !== null ? setIsAuthenticated(true) : setIsAuthenticated(false);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    checkAuthenticated();
+  }, []);
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const setAuth = boolean => {
+    setIsAuthenticated(boolean);
+  };
   return (
-    <div className="App">  </div>
+    <Router>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={props =>
+            !isAuthenticated ? (
+              <SignIn {...props} setAuth={setAuth} />
+            ) : (
+              <Redirect to="/dashboard" />
+            )
+          }
+        />
+        <Route
+          exact
+          path="/login"
+          render={props =>
+            !isAuthenticated ? (
+              <SignIn {...props} setAuth={setAuth} />
+            ) : (
+              <Redirect to="/dashboard" />
+            )
+          }
+        />
+        <Route
+          exact
+          path="/register"
+          render={props =>
+            !isAuthenticated ? (
+              <SignUp {...props} setAuth={setAuth} />
+            ) : (
+              <Redirect to="/dashboard" />
+            )
+          }
+        />
+        <Route
+          exact
+          path="/dashboard"
+          render={props =>
+            isAuthenticated ? (
+              <Dashboard {...props} setAuth={setAuth} />
+            ) : (
+              <Redirect to="/login" />
+            )
+          }
+        />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/Authentication/SignIn.jsx
@@ -0,0 +1,139 @@
+import React, { useState } from "react";
+import { Row, Form, Input, Button, Checkbox } from 'antd';
+import { Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
+import { toast } from "react-toastify";
+// import { Link } from "react-router-dom";
+const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
+
+const SignIn = ({ setAuth }) => {
+    const [btnLoading, setBtnLoading] = useState(false);
+    const [inputs, setInputs] = useState({
+        email: "",
+        password: ""
+    });
+
+    const { email, password } = inputs;
+
+    const onChange = e =>
+        setInputs({ ...inputs, [e.target.name]: e.target.value });
+
+    const onSubmitForm = async e => {
+        e.preventDefault();
+        setBtnLoading(true);
+        try {
+            const body = { email, password };
+            console.log('BODY: ', body);
+            const response = await fetch(
+                "http://localhost:5000/authentication/login",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-type": "application/json"
+                    },
+                    body: JSON.stringify(body)
+                }
+            );
+
+            const parseRes = await response.json();
+
+            if (parseRes.jwtToken) {
+                localStorage.setItem("token", parseRes.jwtToken);
+                setAuth(true);
+                setBtnLoading(false);
+                toast.success("Logged in Successfully");
+            } else {
+                setAuth(false);
+                setBtnLoading(false);
+                toast.error(parseRes);
+            }
+        } catch (err) {
+            setBtnLoading(false);
+            console.error(err.message);
+        }
+    };
+
+
+
+    return (
+        <Row type="flex" justify="center" align="middle" style={{ minHeight: '100vh' }}>
+            <Form
+                name="basic"
+                labelCol={{
+                    span: 8,
+                }}
+                wrapperCol={{
+                    span: 16,
+                }}
+                initialValues={{
+                    remember: true,
+                }}
+                onSubmit={onSubmitForm}
+            >
+                <Form.Item
+
+                label="Email"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your username!',
+                        },
+                    ]}
+                >
+                    <Input
+                       
+                        name="email"
+                        value={email}
+                        onChange={e => onChange(e)}
+                    />
+                </Form.Item>
+
+                <Form.Item
+                    label="Password"
+                   
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your password!',
+                        },
+                    ]}
+                >
+                    <Input.Password
+                       
+                        name="password"
+                        onChange={e => onChange(e)}
+                        value={password}
+                    />
+                </Form.Item>
+
+                <Form.Item
+                    name="remember"
+                    valuePropName="checked"
+                    wrapperCol={{
+                        offset: 8,
+                        span: 16,
+                    }}
+                >
+                    <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+
+
+                <Form.Item
+                    wrapperCol={{
+                        offset: 8,
+                        span: 16,
+                    }}
+                >
+                    {btnLoading ? <Spin indicator={antIcon} /> :
+                        <Button type="primary" htmlType="submit" onClick={onSubmitForm}>
+                            Submit
+                        </Button>
+                    }
+                </Form.Item>
+
+            </Form>
+        </Row>
+    )
+}
+
+export default SignIn
